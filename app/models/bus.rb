24adfb963c6d2c665ea1393a979bca47bf4c1876
Richalex <ricardocldrn@gmail.com>
2017-12-10 19:28:01 -0300

    validacion de patente XXXXDD xxxxDD XXDDDD xxDDDD

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,4 +1,6 @@
 class Bus < ApplicationRecord
-
+  validates_format_of :placa_patente,
+                      :with => /\A([A-Z,a-z]{4}\d{2}|[A-Z,a-z]{2}\d{4})\Z/i,
+                      :message => "no válido."
 
 end
